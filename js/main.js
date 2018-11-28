2302a052ef6198e73fcc1a339c3494b6ce40525f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-28 15:26:56 +0100

    creata e finito controllo per attivare i rossi ed i verdi al click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 //incapsulo il quadrattino
 var quadtratino = $('.row div');
 //eseguo il clik
+console.log(quadtratino.hasClass('green'));
+
 quadtratino.click(function(){
 
 //controllo se quello cliccato possiede la classe
 //red o green
 //in caso positivo gli passo le classi active
-  if(quadtratino.hasClass('red')){
-    alert('rosso');
+  if($(this).hasClass('red')){
+    //alert('rosso');
+    $(this).addClass('activeRed');
+  }
+  else if($(this).hasClass('green')){
+    //alert('verde');
+    $(this).addClass('activeGreen');
   }
 });
