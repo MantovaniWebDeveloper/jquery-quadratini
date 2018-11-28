1639a8313ad8a6472f55c49472adcf76519b7a3a
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-28 16:05:55 +0100

    Creati i contatori funzionanti

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 //incapsulo il quadrattino
 var quadtratino = $('.row div');
+//incapsulo i puntegi
+var punteggioRed = $('#punteggioRed');
+var punteggioGreen = $('#punteggioGreen');
+// creo i contatori per i due colori
+var contatoreRed = 0;
+var contatoreGreen = 0;
 //eseguo il clik
-console.log(quadtratino.hasClass('green'));
-
 quadtratino.click(function(){
-
 //controllo se quello cliccato possiede la classe
 //red o green
 //in caso positivo gli passo le classi active
   if($(this).hasClass('red')){
     //alert('rosso');
     $(this).addClass('activeRed');
+    console.log("CONTATORE RED: " +contatoreRed++);
+    //passo il valore incrementato del punteggio in html
+    punteggioRed.text(contatoreRed++);
   }
   else if($(this).hasClass('green')){
     //alert('verde');
     $(this).addClass('activeGreen');
+    //passo il valore incrementato del punteggio in html
+    console.log("CONTATORE GREEN: " + contatoreGreen++);
+    punteggioGreen.text(contatoreGreen++);
+  }
+  else {
+    console.log("nullo");
   }
 });
